3feecc4c903d1db7212c8eb15114e5e31aa40aef
Christopher Patton <chpatton013@gmail.com>
2016-07-02 12:41:51 -0700

    Sort build targets to defer roads

--- src/action.build.js
+++ src/action.build.js
@@ -3,17 +3,32 @@
 var utilities = require('utilities');
 
 module.exports = function(name, required_body_components) {
-   function get_build_targets(room) {
-      return room.find(FIND_CONSTRUCTION_SITES);
+   function get_high_priority_build_targets(room) {
+      return room.find(FIND_CONSTRUCTION_SITES, {
+         filter: function(target) {
+            return target.structureType != STRUCTURE_ROAD;
+         },
+      });
+   }
+
+   function get_low_priority_build_targets(room) {
+      return room.find(FIND_CONSTRUCTION_SITES, {
+         filter: function(target) {
+            return target.structureType == STRUCTURE_ROAD;
+         },
+      });
    }
 
    return {
       name: name,
       required_body_components: required_body_components,
-      get_build_targets: get_build_targets,
       run: function(creep) {
-         var build_targets = get_build_targets(creep.room);
-         if (build_targets.length) {
+         var build_targets = get_high_priority_build_targets(creep.room);
+         if (build_targets.length == 0) {
+            build_targets = get_low_priority_build_targets(creep.room);
+         }
+
+         if (build_targets.length > 0) {
             var target = utilities.sort_by_distance(
                   build_targets,
                   creep.pos)[0];
